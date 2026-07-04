cecda4d055a733e4f3f711e71bbde41f2c3f8102
Evan Bohart <evangbohart@gmail.com>
2026-07-03 19:57:28 -0700

    full

--- data/data.py
+++ data/data.py
@@ -112,7 +112,7 @@ class Data(Dataset):
             )
 
             targets.append(
-                torch.fill((self.max_transcript_len,), -1, dtype=torch.long, device=self.device)
+                torch.full((self.max_transcript_len,), -1, dtype=torch.long, device=self.device)
             )
 
             for i in range(transcript_len-1):
